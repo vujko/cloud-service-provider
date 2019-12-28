7406a0196e784744fc6bd67879c3e8fad072a703
Vukasin Jokic <vukasinj98@gmail.com>
2019-12-28 22:01:25 +0100

    Added basic navbar

--- WebContent/app/components/homepage.js
+++ WebContent/app/components/homepage.js
@@ -6,11 +6,24 @@ Vue.component("home-page", {
 		    }
 	},
 	template: ` 
-<div>
-	<a href= "#/users">Users</a>
-	<a v-on:click="logout()">Logout</a>
-	
-</div>		  
+	<div>
+		<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+			<a class="navbar-brand" href="#/homepage">Home</a>
+			</div>
+			<ul class="nav navbar-nav">
+			<li><a href="#/users">Users</a></li>
+			<li><a v-on:click="logout()" >Logout</a></li>
+			</ul>
+		</div>
+		</nav>
+		
+		<div class="container">
+		<h3>VM</h3>
+		<p>Prikaz VM</p>
+		</div>
+	</div>
 `
 	, 
 	methods : {
