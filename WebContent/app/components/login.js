572a54c1b978110460895986490f409d1dc81e65
Vukasin Jokic <vukasinj98@gmail.com>
2019-12-30 19:57:04 +0100

    Validation for new org done

--- WebContent/app/components/login.js
+++ WebContent/app/components/login.js
@@ -21,7 +21,7 @@ Vue.component("log-in", {
                         <h4 class="panel-title">Sign in</h4>
                     </div>
                     <div class="panel-body">
-                        <form class="form-signin" role="form">
+                        <form id="login-form" class="form-signin" role="form">
                         <fieldset>  
                             <div class="form-group">
                                 <input class="form-control" placeholder="E-mail" name="email" type="email"  v-model="input.email" required>
@@ -42,21 +42,29 @@ Vue.component("log-in", {
     methods : {
         login : function(){
             var self = this;
-            var userinput = {"email" : '' + this.input.email, "password" : '' + this.input.password};
-            axios
-            .post("/verify", userinput)
-            .then(function(response){ 
-                this.verified = response.data;
-                if(this.verified){
-                    axios
-                    .post("/login",userinput);
-                    self.$router.replace("/homepage");
-                }
-                else{
-                    toast("Wrong email or password.Please try again.");
-                }
-                
-            })
+            var $loginForm = $("#login-form");
+            if( ! $loginForm[0].checkValidity()){
+                $('<input type="submit">').hide().appendTo($loginForm).click().remove();
+            }
+            else{
+                var userinput = {"email" : '' + this.input.email, "password" : '' + this.input.password};
+                axios
+                .post("/verify", userinput)
+                .then(function(response){ 
+                    this.verified = response.data;
+                    if(this.verified){
+                        axios
+                        .post("/login",userinput);
+                        self.$router.replace("/homepage");
+                    }
+                    else{
+                        toast("Wrong email or password.Please try again.");
+                    }
+                    
+                })
+            }
+
+            
         }
         
     }
